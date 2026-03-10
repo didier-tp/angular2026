e8d3fb49b9195f688f3f76c3f145b5e160357d85
Didier Defrance <didier@d-defrance.fr>
2026-03-10 13:29:03 +0100

    s

--- ex_solutions/mars2026/my-app/src/app/basic/basic.component.ts
+++ ex_solutions/mars2026/my-app/src/app/basic/basic.component.ts
@@ -3,12 +3,13 @@ import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 import { TvaComponent } from './tva/tva.component';
 import { MutableComponent } from './mutable/mutable.component';
 import { SimpleSelectorComponent } from '../common/component/simple-selector/simple-selector.component';
+import { TogglePanelComponent } from '../common/component/toggle-panel/toggle-panel.component';
 
 
 
 @Component({
   selector: 'app-basic',
-  imports: [CalculatriceComponent,TvaComponent,MutableComponent,SimpleSelectorComponent],
+  imports: [CalculatriceComponent,TvaComponent,MutableComponent,SimpleSelectorComponent,TogglePanelComponent],
   templateUrl: './basic.component.html',
   styleUrl: './basic.component.css',
 })
