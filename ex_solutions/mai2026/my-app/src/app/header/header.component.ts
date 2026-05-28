6d834badd110858942b8cb32b1a1fa64bc6c5147
Didier Defrance <didier@d-defrance.fr>
2026-05-28 11:05:47 +0200

    input sur header.component.html

--- ex_solutions/mai2026/my-app/src/app/header/header.component.ts
+++ ex_solutions/mai2026/my-app/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,14 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  titre = input("titre_par_defaut");
 
 }
+
+/*
+   input() est un cas particulier de signal()
+   -----
+   Vue externe (dans html du composant parent : <app-header titre="valeur_choisie"></app-header>
+                                      ou bien  <app-header [titre]="variable_title_parent"></app-header>
+                                      ou bien  <app-header [titre]="signal_title_parent()"></app-header>
+*/
