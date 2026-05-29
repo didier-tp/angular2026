177b2067a1cee5e0f7d267ab08c301e66086374f
Didier Defrance <didier@d-defrance.fr>
2026-05-29 14:57:48 +0200

    s

--- ex_solutions/mai2026/my-app/src/app/common/service/tva.service.ts
+++ ex_solutions/mai2026/my-app/src/app/common/service/tva.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class TvaService {
    tva(ht:number ,taux_tva_pct:number){
-    return ht * taux_tva_pct/100.0 
+    return ht * taux_tva_pct/100.0
   }
   ttc(ht:number ,taux_tva_pct:number){
     return ht * (1 + taux_tva_pct/100.0)
